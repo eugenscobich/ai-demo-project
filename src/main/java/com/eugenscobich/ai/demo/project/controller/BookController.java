44aea7740d03ee134e109f025f5bc809237ef1c2
Evgheni Scobici <evghenii.scobici@endava.com>
2025-06-11 15:13:38 +0300

    JKNIG-1 Refactor BookController to use Lombok @RequiredArgsConstructor and add Lombok to dependencies

--- src/main/java/com/eugenscobich/ai/demo/project/controller/BookController.java
+++ src/main/java/com/eugenscobich/ai/demo/project/controller/BookController.java
@@ -2,21 +2,19 @@ package com.eugenscobich.ai.demo.project.controller;
 
 import com.eugenscobich.ai.demo.project.model.Book;
 import com.eugenscobich.ai.demo.project.service.BookService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/books")
+@RequiredArgsConstructor
 public class BookController {
     private final BookService bookService;
-    @Autowired
-    public BookController(BookService bookService) {
-        this.bookService = bookService;
-    }
+    
     @GetMapping
     public List<Book> getBooks() {
         return bookService.getAllBooks();
